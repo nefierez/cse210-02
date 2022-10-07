a03cf542fbbfdd3899fab5f0c993ea9c64dd87cb
greismichell <severer.toucans-0n@icloud.com>
2022-10-06 18:54:53 -0700

    added the def do_updates(self) finction

--- game/director.py
+++ game/director.py
@@ -5,3 +5,21 @@ class Director:
     """A person who directs the game. 
     The responsibility of a Director is to control the sequence of play.
     """
+
+
+    def do_updates(self):
+        #updates the playe's score.
+
+        #Args:
+        #self(Director): An instance of Director.
+
+        if not self.is_playing:
+            return
+
+        for i in range(len(self.card)):
+            cards =self.card[i]
+            cards.display()
+            self.score += cards.points
+            self.total_score += self.score
+
+            

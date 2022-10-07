ac98eae89d7c5db0a280b394bba33fd92d53be9b
rcabanjin <113873622+rcabanjin@users.noreply.github.com>
2022-10-06 19:16:30 -0700

    Ron input

--- game/director.py
+++ game/director.py
@@ -5,3 +5,9 @@ class Director:
     """A person who directs the game. 
     The responsibility of a Director is to control the sequence of play.
     """
+    def start_game(self):
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+            
